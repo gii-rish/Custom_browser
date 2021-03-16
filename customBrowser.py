c6a5739c7b32969f09e5a56e0e2fce642c95d6a7
Girish Mani <girishkrishna97@gmail.com>
2021-03-16 17:59:24 +0530

    added navbar and buttons

--- customBrowser.py
+++ customBrowser.py
@@ -13,8 +13,29 @@ class Browser(QMainWindow):
 		self.setCentralWidget(self.window)
 		self.showMaximized()
 
+		#navbar
+		navBar = QToolBar()
+		self.addToolBar(navBar)
+
+		home, back, front, reloadButton = QAction('Home', self), QAction('Back', self), QAction('Forward', self), QAction('Reload', self)
+		home.triggered.connect(lambda: self.window.setUrl(QUrl("http://google.com")))
+		navBar.addAction(home)
+
+		back.triggered.connect(self.window.back)
+		navBar.addAction(back)
+
+		front.triggered.connect(self.window.forward)
+		navBar.addAction(front)
+
+		reloadButton.triggered.connect(self.window.reload)
+		navBar.addAction(reloadButton)
+
+		# def toHome(self):
+		# 	self.window.setUrl(QUrl("http://google.com"))
+
 
 
 app = QApplication(sys.argv)
+app.setApplicationDisplayName("GfG PyQt5")
 browserWindow = Browser()
 app.exec()
